f97cb238b6aa0db0c5316dc6c26398cf58e987e6
kdebacker <ext.kwinten.de.backer@sofico.be>
2021-04-03 17:45:05 +0200

    first commit

---
+++ api/main.go
@@ -0,0 +1,14 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+}
+
+func main() {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kiwiidb/mastodon-in-twitter-avatar
+
+go 1.16
